81b2a417309f8b24f874afff8fe352619164847b
Marina Corona <marina.c.corona@gmail.com>
2016-01-12 16:51:33 -0700

    making enrollment repo test more enrollment-y

--- test/enrollment_repository_test.rb
+++ test/enrollment_repository_test.rb
@@ -14,34 +14,35 @@ class EnrollmentRepositoryTest < Minitest::Test
 
   def test_find_by_name_returns_nil_for_query_not_in_repo
     # skip
-    e = EnrollmentRepository.new
-    e.load_data({
+    er = EnrollmentRepository.new
+    er.load_data({
     :enrollment => {
       :kindergarten => "./data/Kindergartners in full-day program.csv"
       }
     })
-    find_by_name_output = e.find_by_name("Marshmallows")
+    find_by_name_output = er.find_by_name("Marshmallows")
     assert_nil find_by_name_output
   end
 
   def test_data_in_and_out_is_santized_for_consistency_and_to_be_symbols
     skip
-    e = EnrollmentRepository.new
-    assert_equal :_JAJA, e.form_symbol("-_ jaJA")
-    assert_equal :FFFF, e.form_symbol("@FFff ++~")
-    assert_equal :LOWERCASE, e.form_symbol("lowercase")
+    er = EnrollmentRepository.new
+    assert_equal :_JAJA, er.form_symbol("-_ jaJA")
+    assert_equal :FFFF, er.form_symbol("@FFff ++~")
+    assert_equal :LOWERCASE, er.form_symbol("lowercase")
   end
 
-  def test_find_by_name_returns_district_when_in_repo
+  def test_find_by_name_returns_enrollment_when_in_repo
+    #??????????????????
     skip
-    e = EnrollmentRepository.new
-    e.load_data({
+    er = EnrollmentRepository.new
+    er.load_data({
     :enrollment => {
       :kindergarten => "./data/Kindergartners in full-day program.csv"
       }
     })
-    query = "Colorado"
-    find_by_name_output = e.find_by_name(query)
+    query = "ADAMS-ARAPAHOE"
+    find_by_name_output = er.find_by_name(query)
 
     # assert_equal query.upcase, find_by_name_output.last.last
     assert find_by_name_output.instance_of?(Enrollment)
@@ -49,22 +50,20 @@ class EnrollmentRepositoryTest < Minitest::Test
 
   def test_find_by_name_with_funky_characters_still_returns_district
     skip
-    e = EnrollmentRepository.new
-    e.load_data({
+    er = EnrollmentRepository.new
+    er.load_data({
     :enrollment => {
       :kindergarten => "./data/Kindergartners in full-day program.csv"
       }
     })
-
     query = "ACADEMY 20"
-    find_by_name_output = e.find_by_name(query)
+    find_by_name_output = er.find_by_name(query)
 
-    #
     # assert_equal query.upcase, find_by_name_output.last.last
     assert find_by_name_output.instance_of?(Enrollment)
 
     query ="BRUSH RE-2(J)"
-    find_by_name_output = e.find_by_name(query)
+    find_by_name_output = er.find_by_name(query)
     #
     # assert_equal query.upcase, find_by_name_output.last.last
     assert find_by_name_output.instance_of?(Enrollment)
@@ -72,54 +71,15 @@ class EnrollmentRepositoryTest < Minitest::Test
 
   def test_find_by_name_is_case_insensitive
     skip
-    e = EnrollmentRepository.new
-    e.load_data({
+    er = EnrollmentRepository.new
+    er.load_data({
     :enrollment => {
       :kindergarten => "./data/Kindergartners in full-day program.csv"
       }
     })
-
     query ="brush RE-2(J)"
 
-    assert_equal "AFD", e.find_by_name(query)
+    assert_equal [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1], er.find_by_name(query)
   end
-
-
-  def test_find_all_matching_returns_possible_district_options_for_fragment_search
-    skip
-    e = EnrollmentRepository.new
-    output = e.load_data({
-    :enrollment => {
-      :kindergarten => "./data/Kindergartners in full-day program.csv"
-      }
-    })
-
-    assert_equal ["CENTENNIAL R-1", "CENTER 26 JT"], e.find_all_matching("CEN")
-  end
-
-
-    def test_find_all_matching_returns_possible_district_options_for_fragment_search_lowercase
-      skip
-      e = EnrollmentRepository.new
-      output = e.load_data({
-      :enrollment => {
-        :kindergarten => "./data/Kindergartners in full-day program.csv"
-        }
-      })
-
-      assert_equal ["CENTENNIAL R-1", "CENTER 26 JT"], e.find_all_matching("cen")
-    end
-
-    def test_find_all_matching_returns_empty_array_if_no_matches
-      skip
-      e = EnrollmentRepository.new
-      output = e.load_data({
-      :enrollment => {
-        :kindergarten => "./data/Kindergartners in full-day program.csv"
-        }
-      })
-
-      assert_equal [], e.find_all_matching("XW")
-    end
 
 end
