4748e645060da780240727ceb4e2b9df133fbd69
Marina Corona <marina.c.corona@gmail.com>
2016-01-11 18:53:58 -0700

    adding comments for enrollment, district_repo and district

--- lib/district.rb
+++ lib/district.rb
@@ -1,6 +1,7 @@
 req 'csv'
 
 class District
+  #an instance of this class looks like d = District.new({:name => "ACADEMY 20"})
   attr_reader :enrollment
 
   def initialize({:name => district_name})
