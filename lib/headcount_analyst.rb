2509c3d96431f40c3f6e13f12746ca1671188de8
Marina Corona <marina.c.corona@gmail.com>
2016-01-18 09:00:04 -0700

    added kindergarten participation variation trend method. currently works but gives wrong input

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -9,32 +9,49 @@ class HeadcountAnalyst
   end
 
   def kindergarten_participation_rate_variation(d_name1, against_district)
+    #couldn't we just d_name2 the second object
     d_name2 = against_district.fetch(:against)
     d_object1 = get_district(d_name1)
     d_object2 = get_district(d_name2)
-    average1 = calculate_average_rate(d_object1)
-    average2 = calculate_average_rate(d_object2)
+    average1 = calculate_average_rate_for_all_years(d_object1)
+    average2 = calculate_average_rate_for_all_years(d_object2)
     truncate_float(average1/average2)
     #finds the variance between two districts
     #Example: ha.kindergarten_participation_rate_variation('ACADEMY 20', :against => 'YUMA SCHOOL DISTRICT 1') # => 1.234
   end
 
-  def kindergarten_participation_rate_variation_trend
-    #iterates through the years and puts this in a hash
-    #Example: ha.kindergarten_participation_rate_variation_trend('ACADEMY 20', :against => 'COLORADO') # => {2009 => 0.766, 2010 => 0.566, 2011 => 0.46 }
+  def kindergarten_participation_rate_variation_trend(d_name1, against_district)
+    d_name2 = against_district.fetch(:against)
+    d_object1 = get_district(d_name1)
+    d_object2 = get_district(d_name2)
+    average1 = calculate_average_rate(d_object1, d_object2)
   end
 
-  def get_district(d_name)
-    district_repository.find_by_name(d_name)
+  def calculate_average_rate(d_object1, d_object2)
+   if d_object1.enrollment.kindergarten && d_object2.enrollment.kindergarten
+     data_hash1 = d_object1.enrollment.kindergarten
+     data_hash2 = d_object2.enrollment.kindergarten
+       index = 0
+         while data_hash1.keys[index] == data_hash2.keys[index]
+           annual_enrollment_hash = data_hash1.merge(data_hash2){|key, first, second| truncate_float(first/2.0) + truncate_float(second/2.0) }
+         index += 1
+       end
+
+   end
   end
 
-  def calculate_average_rate(d_object)
+
+  def calculate_average_rate_for_all_years(d_object)
     if d_object.enrollment.kindergarten
       data = d_object.enrollment.kindergarten.values
       data.inject(0) do |memo, datum|
         memo + datum
       end/data.size
     end
+  end
+
+  def get_district(d_name)
+    district_repository.find_by_name(d_name)
   end
 
   def truncate_float(number)
--- test/headcount_analyst_test.rb
+++ test/headcount_analyst_test.rb
@@ -11,7 +11,7 @@ class HeadcountAnalystTest < Minitest::Test
     ha = HeadcountAnalyst.new(dr)
     assert ha.instance_of? HeadcountAnalyst
   end
-  meta ha:true
+  # meta ha:true
   def test_kindergarten_participation_rate_variation_takes_input_of_2_districts_and_returns_variance
     dr = DistrictRepository.new
     dr.load_data({:enrollment => { :kindergarten => "./data/subsets/kindergarten_enrollment.csv"}})
@@ -19,5 +19,15 @@ class HeadcountAnalystTest < Minitest::Test
 
     variation = ha.kindergarten_participation_rate_variation('ACADEMY 20', :against => 'CANON CITY RE-1')
     assert_equal 0.579, variation
+  end
+
+  def test_kindergarten_participation_rate_varaition_trend_tests_for_the_variation_each_year
+      dr = DistrictRepository.new
+      dr.load_data({:enrollment => { :kindergarten => "./data/subsets/kindergarten_enrollment.csv"}})
+      ha = HeadcountAnalyst.new(dr)
+
+      variation = ha.kindergarten_participation_rate_variation_trend('ACADEMY 20', :against => 'COLORADO')
+      variation_hash = {2009 => 0.652}
+      assert_equal variation_hash, variation
   end
 end
